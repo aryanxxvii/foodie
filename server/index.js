51b4d0f8f2b2b37108d0e613b0f6333b865b13f3
Aryan Wadhawan <53692529+aryanxxvii@users.noreply.github.com>
2023-08-04 01:41:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,16 +2,18 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const dotenv = require("dotenv")
-dotenv.config()
 const db = require("./db")
 
 const app = express()
 const productRouter = require("./routes/productRouter")
-var corsOptions = {
-  origin: "https://foo-die.netlify.app",
-}
 
-app.use(cors(corsOptions))
+app.use(
+  cors({
+    origin: "https://foo-die.netlify.app",
+  })
+)
+dotenv.config()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
